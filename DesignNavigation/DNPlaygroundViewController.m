efc216a4ef6d41ce0eb84c833a8b88d17017289c
Ben Norris <ben@bsn.io>
2014-09-24 19:48:49 -0600

    Adjusted style of navigation bar and title

--- DesignNavigation/DNPlaygroundViewController.m
+++ DesignNavigation/DNPlaygroundViewController.m
@@ -27,6 +27,23 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    [self.navigationController.navigationBar setBarTintColor:[UIColor orangeColor]];
+    
+    // This is one way to set the design of the title
+//    [self.navigationController.navigationBar setTitleTextAttributes:
+//        @{NSForegroundColorAttributeName:[UIColor blackColor],
+//          NSFontAttributeName:[UIFont fontWithName:@"Avenir" size:20]}];
+//    self.title = @"Design Testing";
+    
+    // And this is another way to set the title by giving it a label
+    UILabel *titleLabel = [UILabel new];
+    titleLabel.text = @"Halloween";
+    titleLabel.frame = CGRectMake(0, 0, 100, 40);
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.font = [UIFont fontWithName:@"Avenir Next Condensed" size:20];
+    self.navigationItem.titleView = titleLabel;
+    
 }
 
 - (void)didReceiveMemoryWarning
